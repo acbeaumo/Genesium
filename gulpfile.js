e7ca146d65bae772ee411d78caf5bee220d83174
Andrew Beaumont <acbeaumo@gmail.com>
2016-07-31 19:39:30 -0400

    Responsive type; modular scale
    Removed Compass as a dependency. Switched to cssnano for css
    minification. Gulp task "watch" now watches scripts as well.

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,44 @@
-var gulp = require('gulp');
+'use strict';
 
-gulp.task('watch', function() {
-	var compass = require('gulp-compass');
-	gulp.src('./sass/style.scss')
-		.pipe(compass({
-			task: 'watch',
-			sass: './sass',
-			css: './dist'
-		}))
-		.pipe(gulp.dest('./dist'));
-});
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var sass = require('gulp-sass');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 
 gulp.task('styles', function() {
-	var compass = require('gulp-compass'),
-		postcss = require('gulp-postcss'),
-		autoprefixer = require('autoprefixer'),
-		cleancss = require('gulp-clean-css');
-	gulp.src('./sass/style.scss')
-		.pipe(compass({
-			sass: './sass',
-			css: './dist'
-		}))
-		.pipe(postcss([ autoprefixer({
-			browsers: ['last 2 versions']
-		}) ]))
-		.pipe(cleancss())
+	var processors = [
+		autoprefixer({
+			'browsers': ['last 2 versions']
+		}),
+		cssnano({
+			'discardUnused': false,
+			'mergeIdents': false,
+			'reduceIdents': false,
+			'zindex': false
+		})
+	];
+	return gulp.src('./sass/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(postcss(processors))
 		.pipe(gulp.dest('./dist'));
 });
 
 gulp.task('scripts', function() {
-	var concat = require('gulp-concat'),
-		rename = require('gulp-rename'),
-		uglify = require('gulp-uglify');
-	gulp.src('./scripts/*.js')
+	gulp.src('./scripts/**/*.js')
         .pipe(concat('scripts.js'))
 		.pipe(gulp.dest('./dist/js'))
 		.pipe(rename('scripts.min.js'))
 		.pipe(uglify())
         .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('./sass/**/*.scss', ['styles']);
+	gulp.watch('./scripts/**/*.js', ['scripts']);
 });
 
 gulp.task('default', ['styles', 'scripts']);
